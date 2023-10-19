b67eceaf656422f07155d029e3ab02963afd3a7b
Axel <axelpoteau19@gmail.com>
2023-10-19 10:05:58 +0200

    debut affichage pkm

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
+import requests
+from flask_wtf import *
+from wtforms import *
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.secret_key = 'Ma clé secrète'
+
+class PokemonForm(FlaskForm):
+    pokemon = StringField('Pokemon', validators=[DataRequired()])
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=['GET', 'POST'])
+def streetform():
+
+    form = PokemonForm()
+    if form.validate_on_submit():
+        pokemon = form.pokemon.data
+        print(pokemon)
+        r = requests.get("https://api-pokemon-fr.vercel.app/api/v1/pokemon/" + pokemon)
+        pokemon_info = r.json()
+
+        return render_template('show.html', info=pokemon_info)
+
+    return render_template('form.html', form=form)
 
 
 if __name__ == '__main__':

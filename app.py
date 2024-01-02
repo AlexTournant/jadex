86ac1cc42f1c5132361b6d368b472b40344c7f96
Bernard Mattéo <matteo_bernard1@ens.univ-artois.fr>
2024-01-02 22:01:17 +0100

    factorisation du code du projet

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, redirect
 import requests
-from flask_wtf import *
-from wtforms import *
-from wtforms.validators import DataRequired
 from unidecode import unidecode
+
+from bdPokedex import init_db
+from forms.LoginForm import LoginForm
+from forms.PokemonForm import PokemonForm
+from forms.RegisterForm import RegisterForm
 from requeteBD import *
 from insertionBD import *
 
@@ -11,13 +13,13 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'votre_clé_secrète'
 app.secret_key = 'Ma clé secrète'
 
-#USER
-est_connecte=False
-nom=None
-id=None
+# USER
+est_connecte = False
+nom = None
+id = None
 
-class PokemonForm(FlaskForm):
-    pokemon = StringField('Pokemon', validators=[DataRequired()])
+# DB
+init_db()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -32,11 +34,12 @@ def streetform():
             print(pokemon_id)
             r = requests.get(f"https://api-pokemon-fr.vercel.app/api/v1/pokemon/" + str(pokemon_id))
             pokemon_info = r.json()
-            inCollection=False
+            inCollection = False
             if est_connecte:
-                inCollection=isInCollection(pokemon_id,id)
-            return render_template('show.html', info=pokemon_info, form=form, pokemon_names=pokemon_names,nom=nom,est_connecte=est_connecte,inCollection=inCollection,id_pokemon=pokemon_id)
-    return render_template('form.html', form=form, pokemon_names=pokemon_names,nom=nom,est_connecte=est_connecte)
+                inCollection = isInCollection(pokemon_id, id)
+            return render_template('show.html', info=pokemon_info, form=form, pokemon_names=pokemon_names, nom=nom,
+                                   est_connecte=est_connecte, inCollection=inCollection, id_pokemon=pokemon_id)
+    return render_template('form.html', form=form, pokemon_names=pokemon_names, nom=nom, est_connecte=est_connecte)
 
 
 @app.route('/getFileName/<id>')
@@ -49,12 +52,14 @@ def getFileName(id):
     print(son)
     return son
 
+
 def load_pokemon_names():
     r = requests.get("https://api-pokemon-fr.vercel.app/api/v1/pokemon")
     if r.status_code == 200:
         pokemon_data = r.json()
         names = [pkm['name']['fr'] for pkm in pokemon_data]
     return names
+
 
 def get_pokemon_id_by_name(name):
     r = requests.get("https://api-pokemon-fr.vercel.app/api/v1/pokemon")
@@ -65,16 +70,10 @@ def get_pokemon_id_by_name(name):
                 return pkm.get('pokedexId')
     return -1
 
-class LoginForm(FlaskForm):
-    nom = StringField('nom', validators=[DataRequired()])
-    mdp = StringField('mdp', validators=[DataRequired()])
-class RegisterForm(FlaskForm):
-    nom = StringField('nom', validators=[DataRequired()])
-    mdp = StringField('mdp', validators=[DataRequired()])
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    global est_connecte,id,nom
+    global est_connecte, id, nom
     form = PokemonForm()
     pokemon_names = load_pokemon_names()
     if form.validate_on_submit():
@@ -91,13 +90,16 @@ def login():
                                    est_connecte=est_connecte, inCollection=inCollection, id_pokemon=pokemon_id)
     formulaire = LoginForm()
     if formulaire.validate_on_submit():
-        if IsInBD(formulaire.nom.data,formulaire.mdp.data):
-            nom=formulaire.nom.data
+        if IsInBD(formulaire.nom.data, formulaire.mdp.data):
+            nom = formulaire.nom.data
             est_connecte = True
             id = ID_selon_mdp_et_nom(formulaire.nom.data, formulaire.mdp.data)
             return redirect('/')
     message = "c'est pas bon"
-    return (render_template('login.html',pokemon_names=pokemon_names,formulaire=formulaire,form=form,message=message,nom=nom,est_connecte=est_connecte))
+    return (
+        render_template('login.html', pokemon_names=pokemon_names, formulaire=formulaire, form=form, message=message,
+                        nom=nom, est_connecte=est_connecte))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -116,22 +118,25 @@ def register():
                 inCollection = isInCollection(pokemon_id, id)
             return render_template('show.html', info=pokemon_info, form=form, pokemon_names=pokemon_names, nom=nom,
                                    est_connecte=est_connecte, inCollection=inCollection, id_pokemon=pokemon_id)
-    formulaire  = RegisterForm()
+    formulaire = RegisterForm()
     if formulaire.validate_on_submit():
-        print((formulaire.nom.data,formulaire.mdp.data))
-        insertionTableAuthentification((formulaire.nom.data,formulaire.mdp.data))
+        print((formulaire.nom.data, formulaire.mdp.data))
+        insertionTableAuthentification((formulaire.nom.data, formulaire.mdp.data))
         nom = formulaire.nom.data
         est_connecte = True
-        id=ID_selon_mdp_et_nom(formulaire.nom.data,formulaire.mdp.data)
+        id = ID_selon_mdp_et_nom(formulaire.nom.data, formulaire.mdp.data)
         return redirect('/')
-    return render_template('register.html',pokemon_names=pokemon_names,formulaire=formulaire,form=form,est_connecte=est_connecte,nom=nom)
+    return render_template('register.html', pokemon_names=pokemon_names, formulaire=formulaire, form=form,
+                           est_connecte=est_connecte, nom=nom)
+
 
 @app.route('/logout')
 def logout():
-    global est_connecte,nom
-    est_connecte=False
-    nom=None
+    global est_connecte, nom
+    est_connecte = False
+    nom = None
     return redirect('/')
+
 
 @app.route('/collection', methods=['GET'])
 def collection():
@@ -149,31 +154,33 @@ def collection():
                 inCollection = isInCollection(pokemon_id, id)
             return render_template('show.html', info=pokemon_info, form=form, pokemon_names=pokemon_names, nom=nom,
                                    est_connecte=est_connecte, inCollection=inCollection, id_pokemon=pokemon_id)
-    tab=[]
+    tab = []
     for pokemon_id in CollectionAll(id):
         r = requests.get(f"https://api-pokemon-fr.vercel.app/api/v1/pokemon/" + str(pokemon_id))
-        tab.append({"id":pokemon_id,"pokemon":r.json()})
-    return (render_template('collection.html',form=form,collection=tab,pokemon_names=pokemon_names,nom=nom,est_connecte=est_connecte))
+        tab.append({"id": pokemon_id, "pokemon": r.json()})
+    return (render_template('collection.html', form=form, collection=tab, pokemon_names=pokemon_names, nom=nom,
+                            est_connecte=est_connecte))
+
 
 @app.route('/ajoutCollection/<id_pokemon>', methods=['GET'])
 def ajoutCollection(id_pokemon):
     message = "Probleme lors de l'ajout"
     if est_connecte:
-        print(id,type(id),id_pokemon,type(id_pokemon))
+        print(id, type(id), id_pokemon, type(id_pokemon))
         if insertionTableCollection((id_pokemon, id)):
             message = "Bien ajouter"
     print(message)
     return redirect('/')
 
+
 @app.route('/suppCollection/<id_pokemon>', methods=['GET'])
 def suppCollection(id_pokemon):
     message = "Probleme lors de la suppression"
     if est_connecte:
-        if suppPokemon(id_pokemon,id):
-            message="Bien supprimer"
+        if suppPokemon(id_pokemon, id):
+            message = "Bien supprimer"
     print(message)
     return redirect('/')
-
 
 
 if __name__ == '__main__':
--- bdPokedex.py
+++ bdPokedex.py
@@ -1,50 +1,60 @@
 import sqlite3
 from sqlite3 import OperationalError
 
-"""
-Créé la table Collection
-id = identifiant du pokemon 
-date = date a laquelle le pokemon a ete rajouté a la collection
-id_P = id de la personne réferencé
-"""
-try:
-    conn = sqlite3.connect("Pokedex.db")
-    cur = conn.cursor()
-    cur.execute("""
-    create table Collection(
-    id INTEGER PRIMARY KEY ,
-    date date not null,
-    id_P integer not null ,
-     FOREIGN KEY (id_P) REFERENCES Authentification(id)
-    )
-    """)
-except OperationalError:
-    print("La table Collection a deja été créé")
-finally:
-    cur.close()
-    conn.close()
 
-"""
-Créé la table Authentification
-id = identifiant de la personne 
-nom = nom de la personne 
-mdp = mot de passe de la personne 
-"""
-try:
-    conn = sqlite3.connect("Pokedex.db")
-    conn.execute('PRAGMA foreign_keys = ON')
-    cur = conn.cursor()
-    cur.execute("""
-    create table Authentification(
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    nom varchar(15) not null unique ,
-    mdp varchar(20) not null unique ,
-    check ( nom>1 ),
-    check ( mdp>5 )
-    )
-    """)
-except OperationalError:
-    print("La table Authentification a deja été créé")
-finally:
-    cur.close()
-    conn.close()
+def creer_collection():
+    """
+    Créé la table Collection
+    id = identifiant du pokemon
+    date = date a laquelle le pokemon a ete rajouté a la collection
+    id_P = id de la personne réferencé
+    """
+    try:
+        conn = sqlite3.connect("Pokedex.db")
+        cur = conn.cursor()
+        cur.execute("""
+        create table Collection(
+        id INTEGER PRIMARY KEY ,
+        date date not null,
+        id_P integer not null ,
+         FOREIGN KEY (id_P) REFERENCES Authentification(id)
+        )
+        """)
+    except OperationalError:
+        print("La table Collection a deja été créé")
+    finally:
+        cur.close()
+        conn.close()
+
+
+def creer_authentification():
+    """
+    Créé la table Authentification
+    id = identifiant de la personne
+    nom = nom de la personne
+    mdp = mot de passe de la personne
+    """
+    try:
+        conn = sqlite3.connect("Pokedex.db")
+        conn.execute('PRAGMA foreign_keys = ON')
+        cur = conn.cursor()
+        cur.execute("""
+        create table Authentification(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        nom varchar(15) not null unique ,
+        mdp varchar(20) not null unique ,
+        check ( nom>1 ),
+        check ( mdp>5 )
+        )
+        """)
+    except OperationalError:
+        print("La table Authentification a deja été créé")
+    finally:
+        cur.close()
+        conn.close()
+
+
+def init_db():
+    creer_collection()
+    creer_authentification()
+    print("DB initialisée")
